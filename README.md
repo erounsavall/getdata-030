a3c52e06ee4085afeb9a935298003acfb774ffad
erounsavall <erounsavall@users.noreply.github.com>
2015-07-20 13:44:14 -0400

    Various cleanup; working R function and better docs.

--- README.md
+++ README.md
@@ -1,22 +1,54 @@
-# getdata-030
-Course project for Getting Data Coursera course.
+# ReadMe
+Script for reading raw accelerometer data across many files, cleaning it, and producing a tidy data set. 
 
-ReadMe
-Script for pulling accelerometer data, cleaning it, and producing a tidy data set. 
-Assumes data is in a subfolder 'UCI HAR Dataset' of the current working directory 
-Instructions for use: download and run run_analysis.R
+Files:
+run_analysis.R  Script for analyzing data. 
+                link: https://github.com/erounsavall/getdata-030/blob/master/run_analysis.R
+                Assumes data is in a subfolder 'UCI HAR Dataset' of the current working directory 
+                
+codebook.md     Describes the tidy dataset output by the script and how to read the script back into R
+                link: https://github.com/erounsavall/getdata-030/blob/master/codebook.md
+                
+tidydata.txt    The tidy dataset output by the script
+                link: https://github.com/erounsavall/getdata-030/blob/master/tidydata.txt
+                
 
-Refer to Code Book (link) for information on the step-by-step method as well as the description of the resulting tidy data set. 
+Instructions for use: 
+  Download source data to working directory.
+        (link:https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+  Download and run run_analysis.R. 
+  Required packages: plyr, dplyr, reshape2, tidyr
 
-Tidy data set which is output of this script may be found here: [link]
-(and how to read this back into R)
+Extraction Method:
+1. File is downloaded and unzipped to a directory 'UCI HAR Dataset' which is a subfolder of the current working directory
 
+2. Data files in the raw data set:
+  - 'features_info.txt': Description of the variables used on the feature vector.
+  - 'features.txt': List of all features. 561 observations of 2 variables. 
+  - 'activity_labels.txt': Links the class labels with their activity name. 6 observations of 2 variables.
+  - 'train/X_train.txt': Training set. 7352 observations of 561 variables.
+  - 'train/y_train.txt': Training labels. 7352 observations of 1 variable.
+  - 'test/X_test.txt': Test set. 2947 observations of 561 variables.
+  - 'test/y_test.txt': Test labels. 2947 observations of 1 variable.
+  - 'train/subject_train.txt': Subject label. 7352 observations of 1 variable. 
+  - 'test/subject_test.txt': Subject label. 2947 observations of 1 variable.
+  
+3. Approach:
+  Read in feature list and grep to find mean() and SD() measurements.
+  Read in X_train and Y-train as fixed-width files. In initial read, these are 561-column dataframes (the full list of features).
+  Extract only mean() and SD() columns (shrinking data set size significantly)
+  Append subject label columns. 
+  Append activity label columbs.
+  Rbind to create one master data set with 10299 observations of 69 variables.
+  Join with activity lookup to get activity names.
 
-this is the long form as mentioned in the rubric as either long or wide form is acceptable, see https://class.coursera.org/getdata-030/forum/thread?thread_id=107 for discussion
+  'Melt' to put measurement names in rows (the measurement values will be in a column). This is tidier data.
+  Creates large data set with 679734 observations of 4 variables.
+  
+  ddply to summarize by subject, activity name and measurement. 11880 observations of 4 variables. 
 
-Why this is tidy: Single row for each observation, each variable in a column
+Why this is tidy: Single row for each observation, each variable in a column. We were asked to summarize 'the mean of each measurement'. Therefore, I have assumed that 'measurement' is a variable with 66 levels (tBodyAcc-mean()-X, etc), each of which is in its own row.  
 
-narrow (grouped by subject, activity):  Size: rows: 30 * 6 * N, columns: 4
 
 subject1, activity1, measurement 1 name, value
 subject1, activity1, measurement 2 name, value
@@ -27,28 +59,6 @@ subject1, activity2, measurement 2 name, value
 ...
 subject30, activity6, measurement N name, value
 
-Where 'measurement' is either the mean() or std() of various accelerometer measurements, described below. 
+          (Note: this is the long form; as mentioned in the rubric, either long or wide form is acceptable, see https://class.coursera.org/getdata-030/forum/thread?thread_id=107 for discussion
 
-Extraction Method:
-1. File was downloaded and unzipped to a directory 'UCI HAR Dataset' which is a subfolder of the current working directory
-2. Data files used:
-  - 'features_info.txt': Description of the variables used on the feature vector.
-  - 'features.txt': List of all features. 561 observations of 2 variables. 
-  - 'activity_labels.txt': Links the class labels with their activity name. 6 observations of 2 variables.
-  - 'train/X_train.txt': Training set. 7352 observations of 561 variables.
-  - 'train/y_train.txt': Training labels. 7352 observations of 1 variable.
-  - 'test/X_test.txt': Test set. 2947 observations of 561 variables.
-  - 'test/y_test.txt': Test labels. 2947 observations of 1 variable.
-  - 'train/subject_train.txt': Subject label. 7352 observations of 1 variable. 
-  - 'test/subject_test.txt': Subject label. 2947 observations of 1 variable.
-3. Approach:
-  Read in feature list and grep to find mean() and SD() measurements.
-  Read in X_train and Y-train as fixed-width files. 
-  Extract only mean() and SD() columns (shrinking data set size significantly)
-  Append subject label columns. 
-  Append activity label columbs.
-  Rbind to create one master data set with 10299 observations of 69 variables.
-  Join with activity lookup to get activity names.
-  'Melt' to put measurement names in rows (the measurement values will be in a column). This is tidier data.
-  Replace activity labels with descriptive activity names.
-4.                                                              
+                                                   
--- run_analysis.R
+++ run_analysis.R
@@ -1,62 +1,78 @@
-##Start with download file, gzfile()
-##Unzip to a folder 'UCI HAR Dataset' which is a subfolder of the working folder.
+##See README at link: https://github.com/erounsavall/getdata-030/blob/master/README.md
 
 run_analysis <- function() {
   library(plyr)
   library(dplyr)
   library(reshape2)
+  library(tidyr)
   
 ## 1.	Merges the training and the test sets to create one data set.
+## 2.	Extracts only the measurements on the mean and standard deviation for each measurement.  
     ## 'features.txt': List of all features. The length of this list 
-    ## will be the width of the training and test sets.
+    ## will be the original width of the training and test sets before they are narrowed.
+    ## (In other words, the 'features' list represents the columns of the raw training and test data sets)
     features <- read.csv("UCI HAR Dataset/features.txt", col.names=c("index","feature"), sep=" ",header=FALSE)
-    ## 2. Extracting only mean() and SD() features
+    numfeatures <- nrow(features)
+    
+    ## Extracting only mean() and SD() features - will later narrow the training and test sets
+    ## by selecting only these columns. 
     MeanStdColumns <- grep(("mean\\(\\)|std\\(\\)"), features$feature, value = FALSE)
     MeanStdFeatures <- features[features$index %in% MeanStdColumns,]
-    numfeatures <- nrow(features)
-      ## Do we only want the variables representing mean and stdevs?
     
-    ## 'train/X_train.txt': Training set. 7352 obs of 561 vars
+    ## Read in the 'train/X_train.txt': Training set. 7352 obs of 561 vars
     X_train <- read.fwf("UCI HAR Dataset/train/X_train.txt",widths=rep(c(16),times=numfeatures),header=FALSE)
+    
+    ## Subset to pull only the mean() and std() columns we identified above, and apply column names.
+    ## Note: if we applied the full list of column names now, THEN joined and subsetted by grepping column names later
+    ## it would be suboptimal because of duplicate items in the 'features' list which we do not use anyway.
+    ## Also this reduces amount of data to manipulate thereby reducing  processing time. 
     narrX_train <- X_train[,c(MeanStdColumns)]
     colnames(narrX_train) <- MeanStdFeatures[,2]
-    Y_train <- read.csv("UCI HAR Dataset/train/Y_train.txt",col.names="label",header=FALSE)
-    subject_train <- read.csv("UCI HAR Dataset/train/subject_train.txt",col.names="subject",header=FALSE)
     
-    narrX_train$subject <- subject_train$subject  ## add subject
-    narrX_train$activitylabel <- Y_train$label   ## add activity label
-                          
-    ## 'test/X_test.txt': Test set. 2947 obs of 561 vars
+    ##Get the list of activity labels and apply them to the dataframe
+    Y_train <- read.csv("UCI HAR Dataset/train/Y_train.txt",col.names="label",header=FALSE)
+    narrX_train$activitylabel <- Y_train$label   
+    
+    ##And get the list of subject labels and apply them to the data frame
+    subject_train <- read.csv("UCI HAR Dataset/train/subject_train.txt",col.names="subject",header=FALSE)
+    narrX_train$subject <- subject_train$subject  
+                              
+    ## 'test/X_test.txt': Test set. 2947 obs of 561 vars. Repeat the whole process we did for the training set.
     X_test <- read.fwf("UCI HAR Dataset/test/X_test.txt", widths=rep(c(16),times=numfeatures),header=FALSE)
     narrX_test <- X_test[,c(MeanStdColumns)]
     colnames(narrX_test) <- MeanStdFeatures[,2]
-    Y_test <- read.csv("UCI HAR Dataset/test/Y_test.txt",col.names="label",header=FALSE)
-    subject_test <- read.csv("UCI HAR Dataset/test/subject_test.txt",col.names="subject",header=FALSE)
     
-    narrX_test$subject <- subject_test$subject  ## add subject
+    Y_test <- read.csv("UCI HAR Dataset/test/Y_test.txt",col.names="label",header=FALSE)
     narrX_test$activitylabel <- Y_test$label    ## add activity label
     
-    ## merge the two data sets - now we have 10299 obs of 68 vars
-    x <- tbl_df(rbind(narrX_train,narrX_test))
-    x
-
-## 2.	Extracts only the measurements on the mean and standard deviation for each measurement.
-    ## see above
+    subject_test <- read.csv("UCI HAR Dataset/test/subject_test.txt",col.names="subject",header=FALSE)
+    narrX_test$subject <- subject_test$subject  ## add subject
     
+    ## merge the two data sets - now we have 10299 obs of 69 vars
+    x <- tbl_df(rbind(narrX_train,narrX_test))
+   
 ## 3.	Uses descriptive activity names to name the activities in the data set
+    ## Using a 'join' to add the column of activity names from the activity_labels file, matching by the activitylabel 
+    ## index number. Creates data set y with 10299 observations of 70 variables.
     activitylabels <-  read.csv("UCI HAR Dataset/activity_labels.txt", col.names=c("activitylabel","activityname"), sep=" ",header=FALSE)
     y <- join(x, activitylabels, by = "activitylabel")
 
 ## 4.	Appropriately labels the data set with descriptive variable names. 
-    yMelt <- melt(y, id=c("subject", "activityname"), measure.vars = MeanStdFeatures[,2]) 
-    colnames(yMelt) = c("subject","activityname","measurement","value")
 
 ## 5.	From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-    z <- group_by(yMelt, subject, activityname, measurement)
-    tidy <- summarize(y,
-                    subject = distinct(subject),
-                    activityname = distinct(activityname),
-                    measurement = distinct(measurement),
-                    mean = mean(value))
-                          
+    ## First, use 'melt' (reshape2) to pull feature names (the vector being measured) into rows instead of columns. 
+    ## Each observation is in one row and each variable is in one column. I assume 'measurement' is a variable with 66 levels.
+    ## This is "long form" tidy data. See the 'read me' file for more on why this is tidy.
+    
+    yMelt <- melt(y, id=c("subject", "activityname"), measure.vars = MeanStdFeatures[,2]) 
+    colnames(yMelt) = c("subject","activityname","measurement","value")
+    yMelt
+    
+    ## Then, summarize the mean of each measurement for each activity and subject. 
+    ## 11880 rows; to cross check, this is 66 measurements for each of 30 subjects and 6 activities.
+    ## 66*30*6 = 11880
+    tidy <- ddply(yMelt, c("subject", "activityname","measurement"), summarize,
+                   mean = mean(value))
+    ## write.table(tidy,"tidydata.txt")              
+    tidy                     
 }
